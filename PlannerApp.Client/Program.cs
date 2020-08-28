1936e313d8bd729745967921218088520ec92a4f
Shahriaz kobir <shahriazkobir1994@hotmail.com>
2020-08-28 23:19:50 +0600

    Blazor Auth working properly. and auth state and local storage added.

--- PlannerApp.Client/Program.cs
+++ PlannerApp.Client/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using PlannerApp.Shared.Services;
@@ -17,7 +19,14 @@ namespace PlannerApp.Client
             {
                 return new AuthenticationService(URL);
             });
+            builder.Services.AddBlazoredLocalStorage();
             builder.RootComponents.Add<App>("app");
+                       
+            builder.Services.AddOptions();
+
+            builder.Services.AddAuthorizationCore();
+
+            builder.Services.AddScoped<AuthenticationStateProvider, LocalAuthenticationStateProvider>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
